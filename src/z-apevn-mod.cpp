074772857efef02fd041415befeaa9f9e4918f58
Dorrulf <weberwolf84401@gmail.com>
2026-02-08 16:34:29 -0800

    fix: bit checking and stability
    Alerts seemed to be crashing the game a lot, so no longer using them.
    There was a lot of redundant bit setting, should be a lot less now.
    Less reserved and unused array space.
    Now uses the aplocid file - a list of bits that EVN will be allowed to
    send to an AP server.

--- src/z-apevn-mod.cpp
+++ src/z-apevn-mod.cpp
@@ -7,6 +7,10 @@
 #include "Archipelago.h"
 #include <string>
 #include <sstream>
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
 
 // prototype my function so writeArrayToFile understands what it is when called.
 //void writeAdditionals(const FILE *oFile, const int *iArray);
@@ -39,34 +43,61 @@ extern "C" {
 // I would like to initialize it with the current loaded values (ex: reloading a pilot), but that would require hooking in elsewhere. Back to the idea of the menu hotkeys prompting the user for IP to connect? This could be part of that process.
 // If the game is closed and reopenned, thus resetting this, the game *might* emit the same location again. But let's see how the server handles that before we try and bother with caching ourselves.
 // We'll also have to see if the server cares that we sent a check id for a location it doesn't have...
-unsigned char truth_cbs[10000]; //as this is a global declaration, c should initialize the indexes... EVN also 0 indexes their bits
+//unsigned char truth_cbs[10000]; //as this is a global declaration, c should initialize the indexes... EVN also 0 indexes their bits
 //int startingOffset = 2000;	//We care about control bits 2000 - 2999
 //int *dynamicOffset = (int*)0x005914cc;	//this is where EVN stores the dynamic memory offset for the 10000 control bit range
 int* dynamicOffset = (int*)0x005914cc;	//this is where EVN stores the dynamic memory offset for the 10000 control bit range
-size_t mySize = sizeof(truth_cbs)/sizeof(truth_cbs[0]);
+//size_t mySize = sizeof(truth_cbs)/sizeof(truth_cbs[0]);
 
 char ini_path[] = ".\\ap_config.ini";
 
 // looks like c naming convention is _, so this should prob be renamed to is_ap_init
 bool isAPInit = false;
 
-FILE *file;
+// filter file
+char filterFilePath[] = ".\\aplocids.txt";
+// filter map
+std::unordered_map<int, int> emittableBits;
+
+//FILE *file;
+
+int Credits1 = 9900;
+int Credits5 = 9901;
+int Credits10 = 9902;
+int Credits50 = 9903;
+int Credits100 = 9904;
+int Credits500 = 9905;
 
 // EVN - before AP
 //void notifyPlr(std::string message) {
 //void notifyPlr(const char* message) {
 void notifyPlr(char* message) {
 	//TODO: create message in bottom left UI where the dates update on each jump.
-	nv_ShowAlert(message);
+	//nv_ShowAlert(message);	//This may be causing a lot of crashes...
+	std::cout << "Notify Player: " << message << std::endl;
 }
 
-void setBit(short bitOffset) {
+// Sets the given bit. Does NOT care about Received/Emit - check that elsewhere.
+void setBit(int bitOffset) {
+	std::cout << "set bit called for bitOffset: " << bitOffset << std::endl;
+	
+	// Handle special cases
+	if (bitOffset >= Credits1 && bitOffset <= Credits500) {
+		//TODO: give credits.
+		std::cout << "Credits received - TODO: Implement " << std::endl;
+		return;
+	}
 	//int bitMem = *dynamicOffset + startingOffset + (short)i;	//adding our offset
 	//unsigned char* bitMem = *dynamicOffset + startingOffset + (short)i;	//adding our offset
 	//unsigned char* bitMem = *dynamicOffset + bitOffset;	//the offset should actually be baked into the server data, thus the passed int is already adjusted.
 	//*bitOffset = 0x01;	//dereference the pointer based on our offset to get the value at that memory byte. Set it to 1.
 	int* bitMem = reinterpret_cast<int*>(*dynamicOffset + bitOffset);
-	*bitMem = 1;
+	
+	//suppose we try a bit of safety?
+	std::cout << "current bit val: " << *bitMem << std::endl;
+	if (*bitMem == 0) {
+		*bitMem = 1;
+	}
 	
 	/*//void* addr = &(*dynamicOffset + bitOffset);	//generic pointer to memory address
 	void* addr = &(bitMem);	//generic pointer to memory address
@@ -74,9 +105,31 @@ void setBit(short bitOffset) {
 	*byte_ptr = 1	//set the value
 	*/
 	
-	fprintf(file, "set bit: %d \n", bitOffset);
-	
 	return;
+}
+
+bool loadFilterFile() {
+	std::ifstream file(filterFilePath);
+	std::string line;
+	
+	if (!file.is_open()) {
+		std::cerr << "Could not open file: " << filterFilePath << std::endl;
+        return false;
+	}
+	
+	while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        int key;
+
+        // Parse the key from the line
+        if (ss >> key) {
+            // Insert into the unordered_map
+            emittableBits[key] = 0; //set to evn's unset state
+        } else {
+            std::cerr << "Warning: Skipping invalid line format: " << line << std::endl;
+        }
+    }
+    return true;
 }
 
 
@@ -87,18 +140,12 @@ void checkMessages() {
 	
 	while (AP_IsMessagePending()) {
 		AP_Message myMsg = *AP_GetLatestMessage();
-		//fprintf(file, "%s \n", myMsg.text.c_str());
-		//msgStr = std::to_string(myMsg.text);
-		//std::string msgStr = patch::to_string(myMsg.text);
-		//msgStr = myMsg.text; //is already std::string.
-		//need it to be mutable though I guess... c_str returns const, which caused a problem.
-		//const char* msgStr = myMsg.text.c_str();
+		
 		char* msgChar = new char[myMsg.text.length() + 1];
 		strcpy(msgChar, myMsg.text.c_str());
-		fprintf(file, "%s \n", myMsg.text.c_str());
+		std::cout << myMsg.text.c_str() << std::endl;
 		AP_ClearLatestMessage();
 		
-		//notifyPlr(msgStr);
 		notifyPlr(msgChar);
 	}
 	
@@ -108,14 +155,10 @@ void checkMessages() {
 
 // define a callback function
 auto clrCallback = []() {
-	if (file == NULL) {
-		return;
-	}
-	
 	// I'm unsure of the context "clear" here. Action where I should reset progress (bits all to 0), or more like release / complete and flip all bits set to 1?
 	//	TODO: Try releasing the game in console and see if this function get's called I guess.
 	
-	fprintf(file, "AP item clear \n");
+	std::cout << "AP item clear " << std::endl;
 	
 	checkMessages();
 	
@@ -129,27 +172,25 @@ auto irCallback = [](int64_t id, bool notifyPlr) {
 	//	Ships
 	//	Outfits
 	//	Credits
-	if (file == NULL) {
-		return;
-	}
 	
 	if (id > SHRT_MAX || id < SHRT_MIN) {
-		fprintf(file, "!IMPORTANT: ID was out of short range. Conversion done with data loss.");
+		std::cout << "!IMPORTANT: ID was out of short range. Conversion done with data loss." << id << std::endl;
 	}
 	
 	// Let's convert the id to something we can use here
-	short my_short = (short)id;	// possible data loss, but should convert. Do we want to test if the ID was out of range?
+	//short my_short = (short)id;	// possible data loss, but should convert. Do we want to test if the ID was out of range?
 	
 	// For now, there is no real dif between the two cases recieved by Nova. All boils down to control bits.
 	//	Except for credits.
 	//	TODO: IDs for credits (ex: 9999 = 50k credits, 10000 = 500k credits)
 	//		These will alter a different set of memory, so going to save for later.
 	// else, flip bit
-	setBit(my_short);
+	//setBit(my_short);
+	setBit((int)id);
 	
 	// TODO: If notifyPlr, add message string to player UI (where the date is updated after a jump)
 	
-	fprintf(file, "AP item received: %lld %d \n", id, notifyPlr);
+	std::cout << "AP item received: " << id << std::endl;
 	
 	checkMessages();
 	
@@ -159,24 +200,15 @@ auto irCallback = [](int64_t id, bool notifyPlr) {
 // define a callback function
 // Location Checked
 auto lcCallback = [](int64_t id) {
-	// Things like:
-	//	Missions
-	//	Systems?
-	//	Spobs?
-	if (file == NULL) {
-		return;
-	}
+	// I think some kind of confirmation is sent back when we emit, but I'm really not sure what's the purpose of this function in APCpp.
 	
 	if (id > SHRT_MAX || id < SHRT_MIN) {
-		fprintf(file, "!IMPORTANT: ID was out of short range. Conversion done with data loss.");
+		std::cout << "!IMPORTANT: ID was out of short range. Conversion done with data loss." << id << std::endl;
 	}
 	
-	// Let's convert the id to something we can use here
-	short my_short = (short)id;	// possible data loss, but should convert. Do we want to test if the ID was out of range?
+	//setBit((int)id);
 	
-	setBit(my_short);
-	
-	fprintf(file, "AP location checked: %lld \n", id);
+	std::cout << "AP location callback - SKIPPING LC BIT SETTING FOR NOW: " << id << std::endl;
 	
 	checkMessages();
 	
@@ -190,25 +222,25 @@ void doAPInit(){
 	char conn_str[256];
 	DWORD result = GetPrivateProfileStringA("AP EVN", "conn_addr", "", conn_str, sizeof(conn_str), ini_path);
 	if (result == 0) {
-		fprintf(file, "AP CONFIG ERROR: Did not find IP address");
+		std::cerr << "AP CONFIG ERROR: Did not find IP address" << std::endl;
 	}
 	
 	char game_name[64]; //keep username under 64 characters
 	result = GetPrivateProfileStringA("AP EVN", "game_name", "EV Nova", game_name, sizeof(game_name), ini_path);
 	if (result == 0) {
-		fprintf(file, "AP CONFIG ERROR: Did not find valid game name");
+		std::cerr << "AP CONFIG ERROR: Did not find valid game name" << std::endl;
 	}
 	
 	char username[64]; //keep username under 64 characters
 	result = GetPrivateProfileStringA("AP EVN", "username", "", username, sizeof(username), ini_path);
 	if (result == 0) {
-		fprintf(file, "AP CONFIG ERROR: Did not find valid username");
+		std::cerr << "AP CONFIG ERROR: Did not find valid username" << std::endl;
 	}
 	
 	char password[64]; //keep username under 64 characters
 	result = GetPrivateProfileStringA("AP EVN", "password", "", password, sizeof(password), ini_path);
 	if (result == 0) {
-		fprintf(file, "AP CONFIG ERROR: Did not find valid password");
+		std::cerr << "AP CONFIG ERROR: Did not find valid password" << std::endl;
 	}
 	
 	AP_Init(conn_str, game_name, username, password);
@@ -231,20 +263,27 @@ void doAPInit(){
 /* ------- Nova Zone --------*/
 // Functions more directly related to the logic of bit handling
 
-void saveAndEmit(short truthIndex) {
-	truth_cbs[truthIndex] = 1;
-	//AP_Send(startingOffset + truthIndex); //If our control bits start at 2000, but the index is at 100, then we need to send ID 2100
-	//AP_Send(truthIndex);
-	AP_SendItem(truthIndex);	//for some reason, called "SendItem" but expects location ID...
-	//NOTE: AP_Send specifies sending the ID of a location, but we just have the control bits... So we'll have to handle that logic server side.
+void emitBit(int bitOffset) {
+	std::cout << "AP sending val: " << bitOffset << std::endl;
 	
-	// We have to send LOCATION IDs. Not the bits. But, I don't think we can find the mission ID here all that well, based on the control bit. Unless we intercept another function.
-	//	Otherwise, I think we'll have to export a translation file that we can then reference here to do ID lookups (this bit is associated to this mission ID), etc.
+	// Perform actual setstate
+	// This may be overkill because we make one call that has essentially checked this, but just in case this is called from elsewhere:
+	if (emittableBits.count(bitOffset) == 0) {
+		std::cout << "Not allowed to emit bit: " << bitOffset << std::endl;
+		return;
+	}
 	
-	//fprintf(file, "saved and sent bit: %d \n", startingOffset + truthIndex);
-	fprintf(file, "saved and sent bit: %d \n", truthIndex);
+	// NOTE: Currently are not populating this from a saved state post crash / exit
+	//	so this will NOT perfectly prevent duplicate sends, which is why we double check below.
+	if (emittableBits[bitOffset] == 1) {
+		std::cout << "Already sent bit, thus skip: " << bitOffset << std::endl;
+	}
 	
-	return;
+	// Set our truth, then send to AP
+	emittableBits[bitOffset] = 1;
+	AP_SendItem(bitOffset);
+	
+	return;	
 }
 
 /* ------- END --------------*/
@@ -264,88 +303,56 @@ CALL(0x0044807a, _checkBits);
 void checkBits() {
 	// first, reissue the original call so it can process and flip the bits
 	((void (*)())0x00449370)();
-	
+		
 	//todo: move this down or out later, don't need to initialize file first usually
 	// Create a file pointer
     //FILE *file;
 	
-	freopen("stdout.txt", "a", stdout);
-	//freopen("stderr.txt", "w", stderr);
-
-    // Attempt to open the file for writing
-    file = fopen("output", "a");
-    if (file == NULL) {
-        perror("Error opening file");
-        return;
-    }
-	
-	
-	// Check for bit / check if we emit
-	// CONSIDERATIONS:
-	//	We don't have any persistent storage implmented. So what might happen in the following cases:
-	//		1. Connection is lost.
-	//			Upon restore, will APCpp have the message queue'd and retry to send it to the server, or will we have to detect and handle that?
-	//		2. What happens if the game crashes?
-	//			APCpp won't have been able to capture the send most likely. Or even if it did, its cache has been cleared...
-	for (size_t i = 0; i < mySize; i++) {
-		
-		//int bitOffset = *dynamicOffset + startingOffset + (short)i;	//adding our offset
-		//unsigned char cbit = *(unsigned char *)bitOffset;	//dereference the pointer based on our offset to get the value at that memory byte.
-		//unsigned char* bitMem = *dynamicOffset + startingOffset + (short)i;	//adding our offset
-		
-		/*
-		unsigned char* bitMem = *dynamicOffset + (short)i;	//adding our offset
-		unsigned char cbit = *bitOffset;	//dereference the pointer based on our offset to get the value at that memory byte.
-		*/
-		
-		int* bitMem = reinterpret_cast<int*>(*dynamicOffset + i);
-		unsigned char cbit = *bitMem;
-	
-		if (cbit > 0) {
-			if (truth_cbs[i] < 1) {
-				// I think we emit everything, even if it was a local to local effect
-				//	1. This keeps the server on the same page
-				//	2. The return may be a special handled case, such as credits, that we still want to get but aren't handled by bit logic.
-				saveAndEmit(i);
-			}
-		}
-		
-		//TODO: Test if changed from 0 to 1 specifically.
-		
-		// If so, send to our websocket for broadcast to the archipelago server.
-		
-		//fprintf(file, "%x %d %d\n", bitOffset, startingOffset + (short)i, cbit);
-		//fprintf(file, "%x %d\n", (short)i, cbit);
-	}
-	
-	
-	
-	fprintf(file, "init? %d \n", static_cast<int>(isAPInit));
-	
 	AP_ConnectionStatus APStatus = AP_GetConnectionStatus();
 	
-	if (!isAPInit) {
-		fprintf(file, "attempt to init AP");
+	if (!isAPInit) {		
+		freopen("stdout.txt", "a", stdout);
+		freopen("stderr.txt", "a", stderr);
+		
+		loadFilterFile();
+		
+		std::cout << "attempt to init AP" << std::endl;
 		try {
 			doAPInit();
 		}
 		catch (const std::exception& e) {
-			fprintf(file, "exception: %s \n", e.what());
+			std::cerr << "init exception: " << e.what() << std::endl;
 		}
 		catch (...) {
-			fprintf(file, "unknown error");
+			std::cerr << "unknown error" << std::endl;
 		}
 		
 		APStatus = AP_GetConnectionStatus();
 		
-		fprintf(file, "init AP: %d \n", APStatus);
+		std::cout << "AP init: " << (int)APStatus << std::endl;
 		//isAPInit = true;
 		isAPInit = AP_IsInit();
 	}
 	
-	fprintf(file, "connection enum: %d \n", APStatus);
-	//fprintf(file, "callsMade: %d \n", callsMade);
-	
+	// NOTE: The reissue above should let the game naturally set the bit, so let's not worry about setting it again.
+	// Emit the bit flag IF it is part of the list AP server cares about.
+	for (const auto& pair : emittableBits) {
+		if (pair.second == 1) {
+			continue;
+		}
+		
+		int myKey = pair.first;
+		
+		int* bitMem = reinterpret_cast<int*>(*dynamicOffset + myKey);
+		unsigned char cbit = *bitMem;
+		
+		if (cbit > 0) {
+			//let the emit function set the value of emittableBits - in case it is called elsewhere.
+			emitBit(myKey);
+		}
+		
+	}
+			
 	/*if (APStatus == AP_ConnectionStatus::Disconnected || APStatus == AP_ConnectionStatus::ConnectionRefused || callsMade >= callLimit) {
 		AP_Shutdown();
 		fprintf(file, "closing AP: %d \n", APStatus);
